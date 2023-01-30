c00c2706567be77787837ccd74b241034998a36a
Olga Birkovska <birkovskaya92@ukr.net>
2023-01-30 15:44:57 +0200

    test input

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Testing changes</Text>
+       <ImageBackground
+        style={styles.image}
+        source={require("./assets/test-background.jpg")}
+      >
+        <Text style={styles.text}>Let's begin!</Text>
+        <TextInput style={styles.input} textAlign={'center'} />
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +20,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#f0f8ff',
+    marginHorizontal: 40,
+    height: 40,
+    borderRadius: 6,
+    color: "#fff",
+  }
 });
