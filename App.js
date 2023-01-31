3b1795c50d202a488c5d46b6b686ae921546cf1e
Olga Birkovska <birkovskaya92@ukr.net>
2023-01-31 12:47:18 +0200

    login screen

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import {
   View,
   Platform
 } from 'react-native';
-import { RegistrationScreen } from './Screens/RegistrationScreen';
+// import { RegistrationScreen } from './Screens/RegistrationScreen';
+import { LoginScreen } from './Screens/LoginScreen';
 
 export default function App() {
   console.log(Platform.OS);
   return (
     <View style={styles.container}>
-      <RegistrationScreen />
+      {/* <RegistrationScreen /> */}
+      <LoginScreen />
 
       <StatusBar style="auto" />
     </View>
---
+++ Screens/LoginScreen.js
@@ -0,0 +1,108 @@
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  Text,
+  TextInput,
+  TouchableOpacity, 
+  Pressable,
+} from 'react-native';
+import { AntDesign } from "@expo/vector-icons";
+
+export const LoginScreen = () => {
+    return (
+        <View style={styles.container}>
+            <ImageBackground
+                style={styles.background}
+                source={require("../assets/background.jpg")}
+            >
+                {/* Контейнер формы регистрации */}
+                <View style={styles.form}>
+                    <Text style={styles.title}>Войти</Text>
+                    {/* инпут для емейла */}
+                    <TextInput style={styles.input} placeholder='Адрес электронной почты' />
+                    {/* инпут для пароля */}
+                    <TextInput style={styles.input} placeholder='Пароль' secureTextEntry={true} />
+                    {/* Кнопка показать / скрыть пароль */}
+                    <Pressable style={styles.showPass} >
+                        <Text style={styles.showPassText}>Показать</Text>
+                    </Pressable>
+                    {/* Кнопка регистрации */}
+                    <TouchableOpacity activeOpacity={0.8} style={styles.button}>
+                        <Text style={styles.btnTitle}>Войти</Text>
+                    </TouchableOpacity>
+                    {/* ссылка перехода на страницу логина */}
+                    <Pressable style={styles.authNav} >
+                        <Text style={styles.authNavText}>Нет аккаунта? Зарегистрироваться</Text>
+                    </Pressable>
+                </View>
+            </ImageBackground>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    background: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: 'flex-end',
+    },
+    form: {
+        paddingTop: 32,
+        paddingBottom: 111,
+        paddingHorizontal: 16,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        backgroundColor: '#fff',
+    },
+    title: {
+        marginBottom: 32,
+        marginTop: 0,
+        fontSize: 30,
+        color: '#000',
+        textAlign: 'center',
+    },
+    input: {
+        marginBottom: 16,
+        padding: 16,
+        backgroundColor: '#E8E8E8',
+        height: 50,
+        borderRadius: 8,
+        color: "#BDBDBD",
+        fontSize: 16,
+    },
+    showPass: {
+        alignSelf: 'flex-end',
+        marginTop: -50,
+        marginRight: 16,
+    },
+    showPassText: {
+        fontSize: 16,
+        color: '#1B4371',
+    },
+    button: {
+        backgroundColor: '#FF6C00',
+        height: 40,
+        borderRadius: 100,
+        marginTop: 59,
+        marginBottom: 16,
+        padding: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    btnTitle: {
+        color: '#fff',
+        fontSize: 16,
+    },
+    authNav: {
+        textAlign: 'center',
+    },
+    authNavText: {
+        color: '#1B4371',
+        fontSize: 16,
+    }
+})
