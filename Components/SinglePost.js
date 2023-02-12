0e481248f46048e741045369b979d019534b76a2
Olga Birkovska <birkovskaya92@ukr.net>
2023-02-12 12:40:16 +0200

    Update SinglePost.js

--- Components/SinglePost.js
+++ Components/SinglePost.js
@@ -6,6 +6,7 @@ import {
     Pressable
 } from 'react-native';
 import { EvilIcons } from '@expo/vector-icons'; 
+import { AntDesign } from '@expo/vector-icons';
 
 export const SinglePost = () => {
     return (
@@ -14,12 +15,15 @@ export const SinglePost = () => {
             <View style={styles.imgContainer}>
                 <Image />
             </View>
-            <Text>Название</Text>
-            <Pressable>
-                <EvilIcons name="comment" size={24} color="black" />
-                <Text>количество комментариев</Text>
+            <Text style={styles.title}>Название</Text>
+            <Pressable style={styles.commentsBtn}>
+                <EvilIcons name="comment" size={24} color="#BDBDBD" />
+                <Text style={styles.comments}>000</Text>
             </Pressable>
-            <Text>Локация</Text>
+            <Pressable>
+                <AntDesign name="enviromento" size={24} color="#BDBDBD" />
+                <Text>Локация</Text>
+            </Pressable>
         </View>
     );
 };
@@ -33,5 +37,22 @@ const styles = StyleSheet.create({
         height: 240,
         backgroundColor: '#E8E8E8',
         borderRadius: 8
+    },
+    title: {
+        fontFamily: "Roboto-Medium",
+        fontSize: 16,
+        color: '#000',
+        marginTop: 8,
+    },
+    commentsBtn: {
+        marginTop: 8,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    comments: {
+        color: '#BDBDBD',
+        fontFamily: "Roboto-Regular",
+        fontSize: 16,
+        marginLeft: 6,
     }
 })
